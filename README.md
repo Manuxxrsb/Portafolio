979f33485d7ef6f077b99adb124487cad155b0d8
manuxxrsb <manuxxrsb@gmail.com>
2024-11-26 21:21:17 -0300

    Diseno Semi-Responsivo

--- README.md
+++ README.md
@@ -7,13 +7,17 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-----
-----
+---
+
+---
 
 Instalacion de React Vite
 
-1.  ```npm create vite@latest```
-2.  ``` npm install ```
+1.  `npm create vite@latest`
+2.  `npm install`
 
 Ejecuta el proyecto
-1.  ```npm run dev```
+
+1.  `npm run dev`
+
+Proyecto deployado en: https://react-vite-practicas.vercel.app/
--- src/Components/Lenguajes.jsx
+++ src/Components/Lenguajes.jsx
@@ -9,13 +9,13 @@ function Lenguajes() {
             </h2>
           </header>
 
-          <ul className="mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          <ul className="mt-8 grid gap-4 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
             <li>
               <a href="#" className="group block overflow-hidden">
                 <img
                   src="https://img.icons8.com/color/480/golang.png"
                   alt=""
-                  className="h-[35px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[250px]"
+                  className="h-[100px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[150px] lg:h-[200px]"
                 />
                 <div className="relative bg-white pt-3">
                   <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
@@ -30,7 +30,7 @@ function Lenguajes() {
                 <img
                   src="https://img.icons8.com/color/480/html-5--v1.png"
                   alt=""
-                  className="h-[35px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[250px]"
+                  className="h-[100px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[150px] lg:h-[200px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -46,7 +46,7 @@ function Lenguajes() {
                 <img
                   src="https://img.icons8.com/color/480/css3.png"
                   alt=""
-                  className="h-[35px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[250px]"
+                  className="h-[100px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[150px] lg:h-[200px]"
                 />
 
                 <div className="relative bg-white pt-3">
@@ -62,7 +62,7 @@ function Lenguajes() {
                 <img
                   src="https://img.icons8.com/color/480/javascript--v1.png"
                   alt=""
-                  className="h-[35px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[250px]"
+                  className="h-[100px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[150px] lg:h-[200px]"
                 />
 
                 <div className="relative bg-white pt-3">
